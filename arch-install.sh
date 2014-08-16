a96284e0aeb49921866c3d5e377b0e1741c9d82b
She Jinxin <code.falling@gmail.com>
2014-08-16 17:56:37 +0800

    some message

--- arch-install.sh
+++ arch-install.sh
@@ -29,6 +29,10 @@ arch-chroot $ARCH_ROOT /bin/bash -c "${1}"
 }
 #}}}
 
+msg (){
+  echo -e "\033[44;37;5m [INFO] \033[0m ${1}"
+}
+
 #}}}
 
 # file system
@@ -39,6 +43,7 @@ mount $ARCH_TARGET_ROOT $ARCH_ROOT
 
 # pacman base
 pacstrap -i $ARCH_ROOT base base-devel grub-bios
+msg("Base System Installed")
 sleep 2
 
 # fstab
@@ -46,12 +51,13 @@ genfstab -U -p $ARCH_ROOT >> $ARCH_ROOT/etc/fstab
 sleep 2
 
 # Set timezone and hwclock
-arch_chroot 'hwclock --systohc --utc'
+arch_chroot 'hwclock --systohc --localtime'
 sleep 2
 
 # Set hostname
 arch_chroot "echo $ARCH_HOSTNAME > /etc/hostname"
 sleep 2
+
 
 # DHCP start
 arch_chroot 'systemctl enable dhcpcd.service'
@@ -89,7 +95,7 @@ arch_chroot 'os-prober'
 arch_chroot 'grub-mkconfig -o /boot/grub/grub.cfg'
 arch_chroot "grub-install $ARCH_TARGET_DEV"
 sleep 2
-
+msg ("Install packer..")
 # packer
 arch_chroot 'curl https://aur.archlinux.org/packages/pa/packer/PKGBUILD>/tmp/PKGBUILD'
 arch_chroot 'makepkg -s --asroot --noconfirms /tmp/'
@@ -97,5 +103,6 @@ arch_chroot 'pacman -U --noconfirms /tmp/packer*xz'
 
 #rm $ARCH_ROOT/vbox_arch_chroot.sh
 umount $ARCH_ROOT/
-reboot
+msg("All done")
+#reboot
 
