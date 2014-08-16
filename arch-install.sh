33db45516c7ef7d1da8abecd19a0057f49c89d35
She Jinxin <code.falling@gmail.com>
2014-08-16 20:44:53 +0800

    wifi now work

--- arch-install.sh
+++ arch-install.sh
@@ -106,6 +106,8 @@ arch_chroot 'pacman -U --noconfirms /tmp/packer*xz'
 # wifi-meun
 arch_chroot "pacman -S wifi-menu --noconfirm"
 arch_chroot "pacman -S dialog --noconfirm"
+arch_chroot "pacman -S wpa_supplicant --noconfirm"
+
 # swap
 arch_chroot "fallocate -l $ARCH_SWAP_SIZE /swapfile"
 arch_chroot "mkswap /swapfile"
